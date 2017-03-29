9f5aeadad479bb96846d64097906299f75a48a63
Hans Fangohr iota <fangohr@soton.ac.uk>
2017-03-29 12:43:52 +0100

    Add 'main' simulation script, with parameters.

---
+++ sim.py
@@ -0,0 +1,12 @@
+import thelibrary
+
+# Important parameters
+a = 1
+b = 40
+c = 1
+
+# call simulation with important parameters
+results = thelibrary.calculation(a, b, c)
+
+# write result to disk, or here for simplicity to stdout
+print(results)
