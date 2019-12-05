5aeade8014d4caa0089fb9b425c2694416b5f77d
Hans Fangohr <fangohr@soton.ac.uk>
2019-12-05 09:13:26 +0100

    freeze files as were used

---
+++ task.py
@@ -0,0 +1,12 @@
+import thelibrary
+
+# Important parameters
+a = 41.2
+b = 0.7
+c = 0.1
+
+# call simulation with important parameters
+results = thelibrary.calculation(a, b, c)
+
+# write result to disk, or here for simplicity to stdout
+print(results)
